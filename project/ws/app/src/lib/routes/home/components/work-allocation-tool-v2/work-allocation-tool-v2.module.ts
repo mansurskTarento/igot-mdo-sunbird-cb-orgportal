dd1d209f3439ac51c5e8dbfe1d836d355c7ead2f
mansurskTarento <mansur.shaik@tarento.com>
2025-09-04 14:15:48 +0530

    WIP work allocation tool first commit added modules

---
+++ project/ws/app/src/lib/routes/home/components/work-allocation-tool-v2/work-allocation-tool-v2.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { WorkAllocationToolV2RoutingModule } from './work-allocation-tool-v2-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    WorkAllocationToolV2RoutingModule
+  ]
+})
+export class WorkAllocationToolV2Module { }
